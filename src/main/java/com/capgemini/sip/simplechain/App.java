8b99271e9cf47a6416b0b1eba0b60a5be5853d38
Witold Dziadosz <witold.dziadosz@capgemini.com>
2022-07-14 13:34:03 +0200

    Main

---
+++ src/main/java/com/capgemini/sip/simplechain/App.java
@@ -0,0 +1,9 @@
+package com.capgemini.sip.simplechain;
+
+import com.capgemini.sip.simplechain.cli.TrashyCli;
+
+public class App {
+  public static void main(String[] args) {
+    new TrashyCli().loop();
+  }
+}
